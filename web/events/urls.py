e1c16e292532e0d624d3d09f15d9f2c93d2fe1b5
Saikat Das <saikat.das@kaist.ac.kr>
2020-10-10 00:58:56 +0530

    Add modified python backend files of events app.
     - Define the Event model
     - Add registration features
     - Add form for Event model
     - Add create/update/delete methods for Event model
     - Add event description truncation modification feature

---
+++ web/events/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.EventList.as_view(), name='events'),
+    # Place 'event_create' url before 'event_detail' url -> Reason unknown
+    path('create-event/', views.EventCreate.as_view(), name='event_create'),
+    path('<slug>/', views.EventDetail.as_view(), name='event_detail'),
+    path('modify-registration/<pk>/', views.modify_registration, name='modify_event_registration'),
+    path('modify-event/<slug>/', views.EventUpdate.as_view(), name='event_update'),
+    path('delete-event/<slug>/', views.delete_event, name='event_delete'),
+    path('modify-event-truncate-num/<pk>/', views.modify_descr_truncate_num, name='modify_event_descr_truncate_num'),
+    path('modify-event-truncated-descr/<pk>/', views.modify_truncated_descr, name='modify_event_truncated_descr'),
+]
