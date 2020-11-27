6ec487f0304b3e833364610ed70fed74f235212e
Saikat Das <saikat.das@kaist.ac.kr>
2020-11-27 17:52:23 +0530

    Add candidate page, video, and new voting features

---
+++ web/election/static/js/election_embed_video_ratio.js
@@ -0,0 +1,29 @@
+$(function() {
+    var oldval;
+
+    $('.ratio-select').on('focus', function() {
+        oldval = $(this).val();
+    }).on('change', function() {
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('data-embed-ratio-url'),
+            data: {
+                'ratio': $(this).val(),
+                'csrfmiddlewaretoken': CSRF_TOKEN,
+            },
+            success: (response) => {
+                if (response == 'Success') {
+                    let oldclass = `embed-responsive-${ oldval }`;
+                    let newclass = `embed-responsive-${ $(this).val() }`;
+                    $('.embed-responsive').removeClass(oldclass).addClass(newclass);
+                }
+                else {
+                    alert('Error1');
+                }
+            },
+            error: () => {
+                alert('Error0');
+            },
+        });
+    });
+});
