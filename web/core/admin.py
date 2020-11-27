8b3b306905ec28a3fd6081528e8bd6ebb8dfa6b2
Saikat Das <saikat.das@kaist.ac.kr>
2020-11-27 17:32:58 +0530

    Add course resources feaature

--- web/core/admin.py
+++ web/core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+from .models import CourseResources
+
 # Register your models here.
 
 admin.site.site_header = 'KISA Web Administration'
+
+admin.site.register(CourseResources)
