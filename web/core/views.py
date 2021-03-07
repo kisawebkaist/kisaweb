fe8dedcf4b5ea91ce9d0693400a3fe7c44116c0d
beyaldiz <beyaldiz17@gmail.com>
2021-03-07 18:48:43 +0900

    Add homepage features

--- web/core/views.py
+++ web/core/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, HttpResponse, redirect
+from events.models import Event
 
 
 # Create your views here.
 
 def homepage(request):
-    return redirect('election')
+    num_shown_events = 6
+    events = Event.objects.all().order_by("-id")
+    latest_events = events[: min(num_shown_events, len(events))]
+    context = {
+        "event_list": latest_events
+    }
+    return render(request, 'core/homepage.html', context)
 
 
 def course_resources(request):
