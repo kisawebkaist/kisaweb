e257318960a59988634d446d842470e18bc456a4
Saikat Das <saikat.das@kaist.ac.kr>
2021-03-14 23:41:02 +0530

    Add maintenance mode and modify navbar

--- web/core/views.py
+++ web/core/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.conf import settings
 
+from .models import CourseResources
 
 # Create your views here.
 
 def homepage(request):
-    return redirect('election')
+    if settings.MAINTENANCE_MODE == True:
+        return redirect('important_links')
+    else:
+        return redirect('events')
 
+def important_links(request):
+    return render(request, 'core/important_links.html')
 
 def course_resources(request):
     resources = CourseResources.objects.order_by('class_id')
