231e225b5c89819810e59f0e6d86dd9b4ea56e4c
shamsow <sadatshams@gmail.com>
2021-07-11 18:44:44 +0600

    fix window resize bug on some display sizes

--- web/blog/static/js/masonry.js
+++ web/blog/static/js/masonry.js
@@ -1,6 +1,6 @@
 $('.grid').masonry({
 	itemSelector: '.grid-item',
-	columnWidth: 60, // Reducing will decrease the gap between the cards and vice-versa. Will also change the number of cards per row if the change is big.
+	columnWidth: 58, // Reducing will decrease the gap between the cards and vice-versa. Will also change the number of cards per row if the change is big.
 	horizontalOrder: true // Orders the posts from left to right
 	// other options can be added here for further customizations, refer: https://masonry.desandro.com/options.html
   });
