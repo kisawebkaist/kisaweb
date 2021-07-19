4a7ecac0ad97f87f0943327cece2fb9aba14e7ef
unknown <kohirova.sabina@gmail.com>
2021-07-19 15:27:50 +0900

    not done yet

--- web/faq/models.py
+++ web/faq/models.py
@@ -10,12 +10,59 @@ from web.settings import BASE_DIR
 
 
 # Create your models here.
+CATEGORY = (
+    ("", "---"),
+    ("Student life", "Student"),
+    ("Academics", "Academics"),
+    ("Advices", "Advices"),
+    ("nothing", "nothing"),
+)
 
+'''
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE, choices = CATEGORY)
+
+    class Meta:
+        unique_together = ('slug', 'parent',)
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        full_path = [self.name]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' -> '.join(full_path[::-1])
+
+'''
+'''
+class Category(models.Model):
+   name = models.CharField(max_length=255)
+
+   def __str__(self):
+       return self.name
+
+   def get_absolute_url(self): #url name
+       # return reverse('article-detail', args=(str(self.id)))
+       return reverse('home')
+'''
+class Category(models.Model):
+   title_category = models.CharField( max_length= 200 , blank=True, choices = CATEGORY )
 
 class Question(models.Model):
     text = models.TextField()
     timestamp = models.DateTimeField(auto_now=True)
     faq = models.BooleanField()
+    '''
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY,
+        default='nothing'
+    )
+    '''
+    category = models.ForeignKey('Category', blank=True, null=True, on_delete=models.CASCADE, choices=CATEGORY)
     faq_answer = models.TextField(null=True, blank=True)
 
 
