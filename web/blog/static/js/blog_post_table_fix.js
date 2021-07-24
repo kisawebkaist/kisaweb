d222d3beea385095230cbee5817e2cf51393d9c5
mhamzaerol <mehamerol@gmail.com>
2021-07-24 13:37:04 +0900

    Small fix for images in HTMLField

---
+++ web/blog/static/js/blog_post_table_fix.js
@@ -0,0 +1,4 @@
+// $('#blog-content').find('tr').css("height", "");
+// $('#blog-content').find('td').css("height", "");
+// $('#blog-content').find('table').css("table-layout", "fixed");
+// $('#blog-content').find('table').css("font-size", "16px");
