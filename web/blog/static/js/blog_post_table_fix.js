b4c592eb76795d4cb1624457fccd966c248e27b8
mhamzaerol <mehamerol@gmail.com>
2021-07-31 11:58:52 +0900

    Some updates on responsiveness

--- web/blog/static/js/blog_post_table_fix.js
+++
@@ -1,4 +0,0 @@
-// $('#blog-content').find('tr').css("height", "");
-// $('#blog-content').find('td').css("height", "");
-// $('#blog-content').find('table').css("table-layout", "fixed");
-// $('#blog-content').find('table').css("font-size", "16px");
--- web/blog/static/js/masonryInit.js
+++ web/blog/static/js/masonryInit.js
@@ -4,4 +4,4 @@ $('.grid').masonry({
 	horizontalOrder: true, // Orders the posts from left to right
 	fitWidth: true, // In order to center all of the items, rather than aligning them leftward
 	// other options can be added here for further customizations, refer: https://masonry.desandro.com/options.html
-  });
+});
