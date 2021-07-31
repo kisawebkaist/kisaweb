5a172c4822c29bbf27a0f210a8fd0c492bf0de7a
mhamzaerol <mehamerol@gmail.com>
2021-08-01 00:20:01 +0900

    remove collectstatic files

--- web/collectstatic/CACHE/js/events.2a9f2a89500c.js
+++
@@ -1,5 +0,0 @@
-$('#register, #deregister').on('click',function(){let $type=$(this).attr('id');$.ajax({type:'POST',url:$(this).attr('data-event-registration-url'),data:{'type':$type,'csrfmiddlewaretoken':CSRF_TOKEN,},success:(response)=>{if(response=='Success'){if($type=='register'){$(this).attr('id','deregister');$(this).removeClass('btn-success').addClass('btn-secondary');$(this).html('Deregister');}
-else{$(this).attr('id','register');$(this).removeClass('btn-secondary').addClass('btn-success');$(this).html('Register');}
-let $selector=$(this).siblings('#occupancy');let text=$selector.html();let number=parseInt(text.match(/\d+/));let change=($type=='register')?1:-1;if(text.indexOf('seats left')>=0){change*=-1}
-$selector.html(text.replace(/\d+/,String(number+change)));}
-else{alert('Error1');}},error:()=>{alert('Error0');},});});;
--- web/collectstatic/CACHE/js/events.d2880804a3c0.js
+++
@@ -1,6 +0,0 @@
-function readURL(input,$preview){if(input.files&&input.files[0]){var reader=new FileReader();reader.onload=function(e){$preview.attr('src',e.target.result);}
-reader.readAsDataURL(input.files[0]);}}
-$(function(){$event_image_preview=$('#image_preview');height_id='#id_image_height';width_id='#id_image_width';$height=$(height_id);$width=$(width_id);$('#image').change(function(){readURL(this,$event_image_preview);});$('#id_image_contains_information').on('click',function(){$div_id=$('#image_contains_info_dimensions');if($(this).prop('checked')){$div_id.show();}
-else{$height.val($height.attr('value'));$width.val($width.attr('value'));$event_image_preview.height($height.attr('value')).width($width.attr('value'));$div_id.hide();}});$(height_id+', '+width_id).change(function(){$val=$(this).val();if($val<0){alert('Please enter a positive integer');$(this).val($(this).attr('value'));}
-else{$val-=2;if($(this).attr('id')==height_id){$event_image_preview.height($val);}
-else{$event_image_preview.width($val);}}});$('#image_dim_reset').on('click',function(){$height.val($height.attr('value'));$width.val($width.attr('value'));});});;
