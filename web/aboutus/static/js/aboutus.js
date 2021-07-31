705af7597f528366058052b903da54051e01fa06
Jonathan Willianto <jowillianto@gmail.com>
2021-07-31 17:54:50 +0900

    Frontend corrections - Jowi

--- web/aboutus/static/js/aboutus.js
+++ web/aboutus/static/js/aboutus.js
@@ -1,9 +1,15 @@
 var headerName  = ["main", "member", "division", "gallery"]
 var curHeader   = 0;
 for(let i = 1; i < headerName.length; i++) document.getElementById(headerName[i]).style.display = "none"
-
+document.getElementById(`btn-${headerName[curHeader]}`).style["background-color"]   = "var(--gray-dark)"
 function switchHeader(id){
-  document.getElementById(headerName[curHeader]).style.display  = "none"
-  document.getElementById(headerName[id]).style.display         = "flex"
+  var curElem = document.getElementById(headerName[curHeader])
+  curElem.style.display  = "none"
+  curElem = document.getElementById(`btn-${headerName[curHeader]}`)
+  curElem.style["background-color"] = "var(--secondary)"
+  var newElem = document.getElementById(headerName[id])
+  newElem.style.display  = "flex"
+  newElem = document.getElementById(`btn-${headerName[id]}`)
+  newElem.style["background-color"] = "var(--dark)"
   curHeader   = id;
 }
