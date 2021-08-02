e32c8b95256ab25b0b140a86fb6333bb240013ba
Jonathan Willianto <jowillianto@gmail.com>
2021-08-02 09:38:18 +0900

    Finished frontend coding and fixed backend - Jowi

--- web/aboutus/static/js/aboutus.js
+++ web/aboutus/static/js/aboutus.js
@@ -1,15 +1,15 @@
 var headerName  = ["main", "member", "division", "gallery"]
 var curHeader   = 0;
 for(let i = 1; i < headerName.length; i++) document.getElementById(headerName[i]).style.display = "none"
-document.getElementById(`btn-${headerName[curHeader]}`).style["background-color"]   = "var(--gray-dark)"
+document.getElementById(`btn-${headerName[curHeader]}`).disabled  = true;
 function switchHeader(id){
   var curElem = document.getElementById(headerName[curHeader])
   curElem.style.display  = "none"
   curElem = document.getElementById(`btn-${headerName[curHeader]}`)
-  curElem.style["background-color"] = "var(--secondary)"
+  curElem.disabled = false;
   var newElem = document.getElementById(headerName[id])
   newElem.style.display  = "flex"
   newElem = document.getElementById(`btn-${headerName[id]}`)
-  newElem.style["background-color"] = "var(--dark)"
+  newElem.disabled  = true;
   curHeader   = id;
 }
