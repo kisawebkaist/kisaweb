dc2a863d079a7f9bbc0b9d726b22d6836a39b1f7
shamsow <26414605+shamsow@users.noreply.github.com>
2021-09-18 19:54:41 +0600

    added multimedia to navbar

--- web/multimedia/urls.py
+++ web/multimedia/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 import multimedia.views as views
 
 urlpatterns = [
+    path('', views.HomePageView.as_view(), name='multimedia'),
     path('videos/<slug>', views.Video.as_view()), 
     path('images/<slug>', views.Image.as_view()), 
-    path('home', views.HomePageView.as_view()),
-    path('search', views.TagFilter.as_view()),
+    # path('search', views.TagFilter.as_view()),
     path('<slug>', views.MultimediaView.as_view()),
 ]
