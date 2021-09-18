90e2e323b84827c2bc5d88bfb7480a7cd82f472b
shamsow <26414605+shamsow@users.noreply.github.com>
2021-09-18 19:56:31 +0600

    built rough frontend for post display and reworked home view

--- web/multimedia/views.py
+++ web/multimedia/views.py
@@ -1,8 +1,14 @@
 from django.http.response import HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.views import View
+from django.db.models import Count
 from django.http import HttpResponse, JsonResponse, Http404
 import multimedia.models as model
+
+
+RESULTS_PER_PAGE = 5
+
+
 class Video(View):
     def get(self, request, slug):
         video   = model.Video.objects.filter(slug = slug)
@@ -12,7 +18,8 @@ class Video(View):
             return redirect(videoFile)
         else:
             return HttpResponseNotFound
-    
+
+
 class Image(View):
     def get(self, request, slug):
         image   = model.Image.objects.filter(slug = slug)
@@ -22,6 +29,7 @@ class Image(View):
             return redirect(imageFile)
         else:
             return HttpResponseNotFound
+
 
 class MultimediaView(View):
     def get(self, request, slug):
@@ -55,46 +63,51 @@ class MultimediaView(View):
             "date"  : date_created
         }
         return render(request, 'multimedia/multimedia.html', context = context)
+
+
 class HomePageView(View):
     def get(self, request):
-        count       = 5
-        multimedia  = model.Multimedia.objects.all().order_by('date', 'title')
-        if not multimedia.exists():
-            return render(request, "multimedia/multimedia_home.html")
-        else:
-            multimedia = list(multimedia)
-        if count > len(multimedia):
-            count = len(multimedia)
-        multimedia  = multimedia[0:count]
+        tag_data   = self.request.GET.get('tags', '').split(',')
+        multimedia = model.Multimedia.objects.all().order_by('date', 'title')
+
+        if tag_data != ['']:
+            multimedia = multimedia.filter(tags__tag_name__in=tag_data).annotate(num_tags=Count('tags')).filter(num_tags=len(tag_data))
+        print(multimedia)
+
+        # TODO: PAGINATION
+        multimedia  = multimedia[:RESULTS_PER_PAGE]
+
         mediaList   = []
         for media in multimedia:
-            title   = str(media.title)
-            tags    = list(media.tag.all())
-            date    = str(media.date)
-            preview = str(media.previews.file.url)
-            tags    = [str(tag) for tag in tags]
+            title   = media.title
+            tags    = media.tags.all()
+            date    = media.date
+            preview = media.previews.file.url
             mediaList.append({
-                "title"     : title,
-                "tags"      : tags, 
-                "preview"   : preview, 
-                "date"      : date
+                "title"  : title,
+                "tags"   : tags,
+                "preview": preview,
+                "date"   : date
             })
         context = {
-            "multimedia" : mediaList
+            "multimedia": mediaList,
+            'tagObjects': model.MultimediaTag.objects.all(),
         }
         return render(request, "multimedia/multimedia_home.html", context = context)
-    
-class TagFilter(View):
-    #this is to render the tag filtering thing
-    def get(self, request, slug):
-        #searching
-        tag_data    = slug.get('tags', '').split(',')
-        render_data = model.Multimedia.objects.filter(tag__tag_name___in = tag_data).distinct()
-        context = {
-            'tagObjects' : model.MultimediaTags.objects.all(),
-            'render'     : render_data
-        }
-        return render(request, 'multimedia/search.html', context = context)
+
+
+# class TagFilter(View):
+#     #this is to render the tag filtering thing
+#     def get(self, request, slug):
+#         #searching
+#         tag_data    = slug.get('tags', '').split(',')
+#         render_data = model.Multimedia.objects.filter(tag__tag_name___in = tag_data).distinct()
+#         context = {
+#             'tagObjects': model.MultimediaTags.objects.all(),
+#             'render'    : render_data
+#         }
+#         return render(request, 'multimedia/search.html', context = context)
+
 
 #02-02-2002_videoTitle.mp4
 #videoTitle -> 02-02-2002_videoTitle.mp4
