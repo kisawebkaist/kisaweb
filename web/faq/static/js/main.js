a32c2a91d9fbab961193d2b45e842eae3130ce1c
mhamzaerol <mehamerol@gmail.com>
2021-09-23 09:55:30 +0900

    Style adjustments

--- web/faq/static/js/main.js
+++ web/faq/static/js/main.js
@@ -1,4 +1,4 @@
-console.log('Write your JS code here!');
+// console.log('Write your JS code here!');
 $('.faq-heading').click(function () {
 
     $(this).parent('li').toggleClass('the-active').find('.faq-text').slideToggle();
