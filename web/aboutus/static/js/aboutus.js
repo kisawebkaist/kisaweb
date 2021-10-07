cd3d0fa829b6bcb086ee872117c7b50780855ce2
mhamzaerol <mehamerol@gmail.com>
2021-10-07 22:19:59 +0900

    Implemented animation for the navbar switch and some other navbar features (e.g. color/position)

--- web/aboutus/static/js/aboutus.js
+++ web/aboutus/static/js/aboutus.js
@@ -1,15 +1,24 @@
-var headerName  = ["main", "member", "division", "gallery"]
+var headerName  = ["main", "member", "division"]
 var curHeader   = 0;
-for(let i = 1; i < headerName.length; i++) document.getElementById(headerName[i]).style.display = "none"
-document.getElementById(`btn-${headerName[curHeader]}`).disabled  = true;
+
+for(let i = 1; i < headerName.length; i++) {
+  document.getElementById(headerName[i]).style.display = "none"
+}
+
 function switchHeader(id){
+  
+  document.getElementById(`btn-${headerName[curHeader]}`).classList.toggle("active")
+  document.getElementById(`btn-${headerName[id]}`).classList.toggle("active")
+
   var curElem = document.getElementById(headerName[curHeader])
-  curElem.style.display  = "none"
-  curElem = document.getElementById(`btn-${headerName[curHeader]}`)
-  curElem.disabled = false;
   var newElem = document.getElementById(headerName[id])
-  newElem.style.display  = "flex"
-  newElem = document.getElementById(`btn-${headerName[id]}`)
-  newElem.disabled  = true;
-  curHeader   = id;
+
+  $(".aboutus-content").animate({ opacity: 0 }, "slow");
+  $(".aboutus-content").promise().then(() => {
+    curElem.style.display = "none";
+    newElem.style.display = "flex";
+    curHeader   = id; 
+    $(".aboutus-content").animate({ opacity: 1}, "slow");
+  })
+
 }
