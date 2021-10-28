0c46827ed62d339bb017f32cfefa0ef569fc9a04
mhamzaerol <mehamerol@gmail.com>
2021-10-28 20:50:28 +0900

    Update views.py, implement card view for the member list

--- web/aboutus/views.py
+++ web/aboutus/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
 
 from .models import MainContent, Member, InternalBoardMember, DivisionDescription
 
 def aboutus(request):
+    current_date = datetime.today()
+    year, semester = current_date.year, ('Fall' if current_date.month > 6 else 'Spring')
+    
+    internal_board_members = InternalBoardMember.objects.filter(year=year, semester=semester)
+    members = Member.objects.filter(year=year, semester=semester)
+
     context = {
         'main_contents'             : MainContent.objects.all(),
-        'internal_board_members'    : InternalBoardMember.objects.all(),
-        'members'                   : Member.objects.all(),
         'division_descriptions'     : DivisionDescription.objects.all(),
+        'internal_board_members'    : internal_board_members,
+        'members'                   : members,
     }
     return render(request, 'aboutus/aboutus.html', context)
