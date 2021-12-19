8a6f20172fd1e8b1baabe5d00bb8a4608dde1c93
shamsow <26414605+shamsow@users.noreply.github.com>
2021-12-19 18:32:47 +0600

    Make changes to prepare for important links app

--- web/core/urls.py
+++ web/core/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('important-links', views.important_links, name='important_links'),
+    # path('important-links', views.important_links, name='important_links'),
 
     path('events/', include('events.urls')),
     path('election/', include('election.urls')),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('docs/', include('docs.urls')),
     path('sso/', include('sso.urls')),
     path('blog/', include('blog.urls')),
+    path('important-links/', include('important_links.urls')),
 ]
