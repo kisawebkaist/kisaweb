8a369c8956af859715ea3a217b45415cde5e1927
mhamzaerol <mehamerol@gmail.com>
2021-12-23 18:10:25 +0900

    Small change with election visibility

--- web/election/context_processors.py
+++ web/election/context_processors.py
@@ -4,10 +4,13 @@ from django.utils import timezone
 from election.models import Election
 
 def navbar_election_link_visible(request):
-    latest_election = Election.objects.latest('start_datetime')
-    # visible = now >= latest_election.start_datetime and now <= latest_election.end_datetime
-    visible = latest_election.is_open
+    if Election.objects.all().exists():
+        latest_election = Election.objects.latest('start_datetime')
+        # visible = now >= latest_election.start_datetime and now <= latest_election.end_datetime
+        visible = latest_election.is_open
+    else:
+        visible = False
     return {
         'election_link_visible': visible,
     }
-    return True
+    # return True
