4ca6e68e144ce07ce824e90939872b6d84bc2974
mhamzaerol <mehamerol@gmail.com>
2022-01-06 15:21:33 +0900

    SSO context processor bugfix + election re-migrate

--- web/sso/context_processors.py
+++ web/sso/context_processors.py
@@ -1,12 +1,14 @@
 import yaml
 
+import os
 from .models import LoginError
+from web.settings import BASE_DIR
 
 def login_error(request):
     if LoginError.objects.all().exists():
         login_error_object = LoginError.objects.all()[0]
     else:
-        with open('data/required.yaml') as f:
+        with open(os.path.join(BASE_DIR, 'data/required.yaml')) as f:
             parsed = yaml.safe_load(f)
             fields = next((d for d in parsed if d['model']=='sso.models.LoginError'))['fields']
             login_error_object = LoginError.objects.create(**fields)
