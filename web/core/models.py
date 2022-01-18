828ed7cbdff273803b1d5a7d5ce0f45bc7730a09
shamsow <26414605+shamsow@users.noreply.github.com>
2022-01-18 19:46:29 +0600

    Add separator validator to Category abstract model

--- web/core/models.py
+++ web/core/models.py
@@ -7,11 +7,11 @@ from tinymce.models import HTMLField
 from django.core.validators import RegexValidator
 
 # Validators
-tag_validator = RegexValidator(r'[^\w\-]', inverse_match=True, message='Spaces and punctuation (except "-" and "_") are not allowed.')
+separator_validator = RegexValidator(r'[^\w\-]', inverse_match=True, message='Spaces and punctuation (except "-" and "_") are not allowed.')
 
 # Abstract Classes
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=50, blank=False, unique=True, validators=[tag_validator])
+    tag_name = models.CharField(max_length=50, blank=False, unique=True, validators=[separator_validator])
     def __str__(self):
         return self.tag_name
 
@@ -44,7 +44,7 @@ class Content(models.Model):
         abstract = True
 
 class Category(models.Model):
-    title_category = models.CharField(max_length= 200, blank=True, unique=True)
+    title_category = models.CharField(max_length= 200, blank=True, unique=True, validators=[separator_validator])
     
     def __str__(self):
         return self.title_category
