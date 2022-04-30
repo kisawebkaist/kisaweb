2fe9903c3e11f9b7a399795037a0cac9f2e1597a
AKOTET YESHAW TESEMA <akotet@MacBook-Pro-3.local>
2022-04-30 14:30:09 +0900

    role back

--- web/web/settings.py
+++ web/web/settings.py
@@ -18,7 +18,6 @@ from django.conf.global_settings import DATETIME_INPUT_FORMATS
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
