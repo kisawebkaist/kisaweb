cd2588540af839f30588afbc36b8cc15e298c07e
Akotet <akiyeshaw@gmail.com>
2022-04-30 16:36:52 +0900

    Revert "role back"
    This reverts commit 2fe9903c3e11f9b7a399795037a0cac9f2e1597a.

--- web/web/settings.py
+++ web/web/settings.py
@@ -18,6 +18,7 @@ from django.conf.global_settings import DATETIME_INPUT_FORMATS
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
