6bb059375daa1aeebff92490b4969e1fa79b49ff
jowillianto <jowillianto@kaist.ac.kr>
2022-09-07 01:31:50 +0900

    [#95] Added Visitor Tracking

--- web/url_shortener/views.py
+++ web/url_shortener/views.py
@@ -4,10 +4,16 @@ from django.http import HttpRequest
 
 from .models import UrlShortener
 
+def get_ip_address(request : HttpRequest) -> str:
+    try:
+        return request.META['HTTP_X_FORWARDED_FOR'].split(',')[0]
+    except KeyError:
+        return request.META['REMOTE_ADDR']
+
 class URLShortenerView(View):
     def get(self, request : HttpRequest, name : str):
-        url     = UrlShortener.objects.filter(
-            name = name
-        )
-        url     = get_object_or_404(url)
+        url         = UrlShortener.objects.filter(name = name)
+        url         = get_object_or_404(url)
+        ip_address  = get_ip_address(request)
+        url.visit(ip_address)
         return redirect(to = url.target)
