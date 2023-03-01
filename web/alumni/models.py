414960574163d2ca5afa776f914bc14fe08ebb89
n4-t3 <nathanmekuria@gmail.com>
2023-03-01 10:50:24 +0900

    added year validators for alumni page

--- web/alumni/models.py
+++ web/alumni/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
 # Create your models here.
 
 class Kisa_Position(models.Model):
@@ -15,10 +17,10 @@ class Alumni(models.Model):
     ]
     name = models.CharField(max_length=255,blank=False,null=False)
     joined_season = models.CharField(choices=SEASON_CHOICES,max_length=6,default='Spring',blank=False,null=False)
-    joined_year = models.IntegerField(default=2023)
+    joined_year = models.IntegerField(default=2004,blank=False,null=False, validators=[MinValueValidator(2004), MaxValueValidator(datetime.date.today().year)])
     separated_season = models.CharField(choices=SEASON_CHOICES, max_length=6,blank=True,null=True,help_text='If the alumni only worked for one season please leave this part empty.')
-    separated_year = models.IntegerField(blank=True,null=True, help_text='If the alumni only worked for one season please leave this part empty.')
-    worked_positions = models.ManyToManyField(KISA_Positions, blank=False)
+    separated_year = models.IntegerField(blank=True,null=True, validators=[MinValueValidator(2004), MaxValueValidator(datetime.date.today().year)], help_text='If the alumni only worked for one season please leave this part empty.')
+    worked_positions = models.ManyToManyField(Kisa_Position, blank=False)
     current_contact = models.URLField(blank=True,null=True,help_text='Current contact of the Alumni. Be sure to ask the alumni for permission to post it.')
     def __str__(self):
         return self.name
