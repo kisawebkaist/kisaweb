86314bd82bd886209b85d9a11dacca26afc9a81b
Zwe Myint Mo <momyintzwe@gmail.com>
2023-11-10 20:04:55 +0900

    corrections again :3

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,36 +4,36 @@ import Footer from "../core/footer"
 import fakeNavbarData from "../core/fakeDataForNavbar"; // Import the fake data
 import fakeFooterData from "../core/fakeDataForFooter";
 
-import NavEntryT from "../core/navbar-type";
-import { useState, useEffect } from "react";
-import { FooterT } from "../API/misc";
+// import NavEntryT from "../core/navbar-type";
+// import { useState, useEffect } from "react";
+// import { FooterT } from "../API/misc";
 
-// Zwe: Just testing, feel free to delete this
-const api_endpoint = process.env.REACT_APP_API_ENDPOINT;
+// // Zwe: Just testing, feel free to delete this
+// const api_endpoint = process.env.REACT_APP_API_ENDPOINT;
 
-function Home() {
-  const [navbar, setNavbar] = useState<NavEntryT[]>([]);
-  const [footer, setFooter] = useState<FooterT>();
+// function Home() {
+//   const [navbar, setNavbar] = useState<NavEntryT[]>([]);
+//   const [footer, setFooter] = useState<FooterT>();
 
-  useEffect(()=>{
-    fetch(api_endpoint+"misc/navbar")
-    .then((res) => res.json())
-    .then((json) => setNavbar(json))
+//   useEffect(()=>{
+//     fetch(api_endpoint+"misc/navbar")
+//     .then((res) => res.json())
+//     .then((json) => setNavbar(json))
 
-    fetch(api_endpoint+"misc/footer")
-    .then((res) => res.json())
-    .then((json) => setFooter(json))
+//     fetch(api_endpoint+"misc/footer")
+//     .then((res) => res.json())
+//     .then((json) => setFooter(json))
 
-  }, []);
-  return (
-    <Navbar config={navbar} />
-  );
-}
-
-// const Home = () => {
+//   }, []);
 //   return (
-//     <Navbar config = {fakeNavbarData} />
-//   )
+//     <Navbar config={navbar} />
+//   );
 // }
+
+const Home = () => {
+  return (
+    <Navbar config = {fakeNavbarData} />
+  )
+}
 
 export default Home
