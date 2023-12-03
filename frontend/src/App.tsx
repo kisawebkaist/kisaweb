62143f7d799a485d61d7a9e996e1043120b1c5ed
Jonathan Willianto <jowillianto@gmail.com>
2023-12-03 11:30:11 +0900

    [#165] Use ubuntu font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider, createTheme } from "@mui/material"
 import KisaRoutes from "./routes/routes"
 import { BrowserRouter } from "react-router-dom"
 import '@fontsource/roboto/300.css';
@@ -5,12 +6,26 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Ubuntu",
+      "Roboto",
+      "times new roman",
+      "times",
+      "roman",
+      "serif"
+    ].join(','),
+  },
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <KisaRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme = {theme}>
+      <BrowserRouter>
+        <KisaRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
