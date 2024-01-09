6cdfd5ca8dfc38a33a254f724b60f954c8ae7f64
Zwe Myint Mo <momyintzwe@gmail.com>
2024-01-10 05:58:28 +0900

    removed some views from sso login, changed methods to POST, added Origin header check

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,8 +5,8 @@ from web.settings import URL_SHORTENER_PREFIX
 from . import views
 
 urlpatterns = [
-    # path('', views.homepage, name='homepage'),
-    # path('important-links', views.important_links, name='important_links'),
+    path('', views.homepage, name='homepage'),
+    path('important-links', views.important_links, name='important_links'),
 
     path('events/', include('events.urls')),
     path('election/', include('election.urls')),
--- backend/core/views.py
+++ backend/core/views.py
@@ -9,20 +9,25 @@ import rest_framework.status
 
 # Create your views here.
 
-# def homepage(request):
-#     if settings.MAINTENANCE_MODE == True:
-#         return redirect('important_links')
-#     else:
-#         num_shown_events = 6
-#         events = Event.objects.all().order_by("-id")
-#         latest_events = events[: min(num_shown_events, len(events))]
-#         division_list = DivisionContent.objects.all()
-#         colors = ['red', 'green', 'orange', 'blue', 'black', 'purple']
-#         context = {
-#             "event_list": latest_events,
-#             "division_list": list(zip(division_list, colors)),
-#         }
-#         return render(request, 'core/homepage.html', context)
+def homepage(request):
+    if settings.MAINTENANCE_MODE == True:
+        return redirect('important_links')
+    else:
+        num_shown_events = 6
+        events = Event.objects.all().order_by("-id")
+        latest_events = events[: min(num_shown_events, len(events))]
+        division_list = DivisionContent.objects.all()
+        colors = ['red', 'green', 'orange', 'blue', 'black', 'purple']
+        context = {
+            "event_list": latest_events,
+            "division_list": list(zip(division_list, colors)),
+        }
+        return render(request, 'core/homepage.html', context)
+    
+misc_endpoints = {
+            'navbar': NavBar,
+            'footer': Footer,
+        }
 
 def important_links(request):
     return render(request, 'core/important_links.html')
