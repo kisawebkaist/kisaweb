45d540a53e849f5db41741a480c4cc469569a16a
Jonathan Willianto <jowillianto@gmail.com>
2024-01-10 09:04:12 +0900

    [#165] urls config for about us

--- backend/aboutus/urls.py
+++ backend/aboutus/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-    path('', views.aboutus, name='aboutus'),
-]
+# urlpatterns = [
+#     path('', views.aboutus, name='aboutus'),
+# ]
+
+router = DefaultRouter()
+router.register(
+  r'members', views.MemberViewset, basename = 'aboutus-members'
+)
+router.register(
+  r'internal-members', views.InternalBoardMemberViewset,
+  basename = 'aboutus-internal-members'
+)
+router.register(
+  r'divisions', views.DivisionViewset, basename = 'aboutus-divisions'
+)
+urlpatterns = router.urls
