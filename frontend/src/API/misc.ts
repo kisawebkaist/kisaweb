6a3be33b2fa042c099598d0a3e0a82a68b709f69
Passavee Los <passavee.losripat@gmail.com>
2024-10-04 23:50:34 +0900

    Fix fakedata for navbar and footer issues

--- frontend/src/API/misc.ts
+++ frontend/src/API/misc.ts
@@ -1,11 +1,7 @@
  import axios from "axios";
-import fakeNavbarData from "../core/fakeDataForNavbar";
-import fakeFooterData from "../core/fakeDataForFooter";
 
 export default class MiscAPI {
   static query = <T> (path: string,fallback: () => T): Promise<T> => axios.get(`${process.env.REACT_APP_API_ENDPOINT}/misc/${path}`).then((res)=>res.data, fallback);
-  static navbar = () => MiscAPI.query("navbar", () => fakeNavbarData);
-  static footer = () => MiscAPI.query("footer", () => fakeFooterData);
 }
 
 class MetaDatabase{
