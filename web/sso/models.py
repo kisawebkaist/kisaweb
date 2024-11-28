75d118f19efd1a28c1e2694e7031d41bfd631b14
Web Division <web-div@kisa.kaist.ac.kr>
2024-11-28 19:04:52 +0900

    [Fix] Update user info upon login
    Outdated user info can interfere with election condition checks

--- web/sso/models.py
+++ web/sso/models.py
@@ -1,5 +1,8 @@
+import secrets
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import transaction
 
 from tinymce.models import HTMLField
 
@@ -52,6 +55,69 @@ class User(AbstractUser):
     sso_id = models.CharField(max_length=100, blank=True, null=True)  # uid
     student_department_name_english = models.CharField(max_length=100, blank=True, null=True)  # acad_ebs_org_name_eng
     student_department_name_korean = models.CharField(max_length=100, blank=True, null=True)  # acad_ebs_org_name_kor
+
+    KSSO_KEYS_AND_FIELDS = [
+        ('kaist_uid', 'kaist_uid'), 
+        ('ku_kname', 'korean_name'), 
+        ('displayname', 'full_name'), 
+        ('sn', 'first_name'), 
+        ('givenname', 'last_name'),
+        ('ku_born_date', 'dob'), 
+        ('c', 'nationality'), 
+        ('ku_sex', 'sex'),
+        ('mail', 'kaist_email'), 
+        ('ku_ch_mail', 'external_email'),
+        ('ku_employee_number', 'employee_number'), 
+        ('ku_std_no', 'student_number'), 
+        ('ku_acad_org', 'bachelors_department_code'), 
+        ('ku_acad_name', 'bachelors_department_name'), 
+        ('ku_campus', 'campus'),
+        ('title', 'title_english'), 
+        ('ku_psft_user_status', 'student_status_english'), 
+        ('ku_psft_user_status_kor', 'student_status_korean'),
+        ('ku_acad_prog_code', 'degree_code'), 
+        ('ku_acad_prog', 'degree_name_korean'), 
+        ('ku_acad_prog_eng', 'degree_name_english'),
+        ('employeeType', 'user_group'), 
+        ('ku_prog_effdt', 'student_admission_datetime'), 
+        ('ku_stdnt_type_id', 'student_type_id'), 
+        ('ku_stdnt_type_class', 'student_type_class'), 
+        ('ku_category_id', 'student_category_id'),
+        ('ku_prog_start_date', 'student_enrollment_date'), 
+        ('ku_prog_end_date', 'student_graduation_date'),
+        ('acad_ebs_org_id', 'student_department_id'), 
+        ('uid', 'sso_id'),
+        ('acad_ebs_org_name_eng', 'student_department_name_english'), 
+        ('acad_ebs_org_name_kor', 'student_department_name_korean'),
+    ]
+
+    @classmethod
+    def from_info_json(cls, user_info: dict):
+        query = cls.objects.filter(kaist_uid=user_info['kaist_uid'])
+        with transaction.atomic():
+            if query.exists():
+                return query[0].update_from_info_json(user_info)
+            user = cls()
+            for key, field in cls.KSSO_KEYS_AND_FIELDS:
+                if key in user_info:
+                    setattr(user, field, user_info[key])
+            user.username = user_info.get('full_name', user.get_full_name()) # sometimes the displayname is not there
+            for c in "!#$%^&*(),./<>?\\|":
+                user.username = user.username.replace(c, '')
+            user.username = user.username.strip()
+            user.username = user.username.replace(' ', '_') + '_' + secrets.token_hex(4)
+            user.set_unusable_password()
+            user.full_clean()
+            user.save()
+        return user
+        
+    def update_from_info_json(self, user_info: dict):
+        for key, field in self.KSSO_KEYS_AND_FIELDS:
+            if key in user_info:
+                setattr(self, field, user_info[key])
+        self.full_clean()
+        self.save()
+        return self
 
 class LoginError(models.Model):
     email = models.EmailField()
--- web/sso/views.py
+++ web/sso/views.py
@@ -1,5 +1,6 @@
 import os
 from time import time
+from django.core.exceptions import SuspiciousOperation, ValidationError
 from django.http.response import HttpResponseRedirect
 import requests
 import json
@@ -20,6 +21,7 @@ from django.views.decorators.http import require_http_methods
 
 
 from django.utils.decorators import method_decorator
+from django.utils.translation import gettext as _
 from django.views.decorators.csrf import csrf_exempt
 
 import base64
@@ -72,6 +74,14 @@ keys_and_fields = [
     ('acad_ebs_org_name_kor', 'student_department_name_korean'),
 ]
 
+def ensure_relative_url(url):
+    if url[0] == '/':
+        return url
+    url = urllib.parse.urlparse(url)._replace(scheme='', netloc='').geturl()
+    if url == "" or url[0] != '/':
+        url = '/' + url
+    return url
+
 def decrypt(data, state, host) :
     BS = AES.block_size 
     unpad = lambda s : s[0:-s[-1]] 
@@ -110,6 +120,11 @@ def login_view(request):
 @csrf_exempt
 def login_response_view(request):
 
+    origin = request.META.get('HTTP_ORIGIN')
+    if origin != "https://iam2.kaist.ac.kr":
+        useragent = request.META.get('HTTP_USER_AGENT')
+        raise SuspiciousOperation(_(f'CSRF Failed: Origin checking failed - {origin} does not match any trusted origins. Sent by {useragent}'))
+
     if bool(request.POST.get('success')):
 
         params = {
@@ -141,28 +156,19 @@ def login_handler_view(request):
         return redirect('login-error')
     
     result = decrypt(context.get('raw_result'), context.get('state'), context.get('http_host') [:2]).decode('utf-8')
-    result = json.loads(result, encoding='utf-8')
 
-    user_info = result['dataMap']['USER_INFO']
-        
-    if not User.objects.filter(pk=user_info['kaist_uid']).exists():
-        user_params = {}
-        for key, field in keys_and_fields:
-            if key in user_info:
-                user_params[field] = user_info[key]
-        username = user_params['full_name']
-        for c in "!#$%^&*(),./<>?\|":
-            username = username.replace(c, '')
-        username = username.strip()
-        username = username.replace(' ', '_')
-        user_params['username'] = f"{username}_{secrets.token_hex(4)}"
-        user = User(**user_params)
-        user.save()
-    else:
-        user = User.objects.get(pk=user_info['kaist_uid'])
-
-    login(request, user)
-    return redirect(next)
+    try:
+        result = json.loads(result)
+        user = User.from_info_json(result['dataMap']['USER_INFO'])
+        login(request, user)
+    except KeyError as e:
+        raise SuspiciousOperation(f'Key error {e} using {result}.')
+    except json.JSONDecodeError as e:
+        raise SuspiciousOperation(f'JSON decoding failed {e} using {result}')
+    except ValidationError as e:
+        raise SuspiciousOperation(f'User model validation failed {e} using {result}')
+    
+    return redirect(ensure_relative_url(next))
 
 def login_error_view(request):
     return render(request, 'sso/login_error.html', {})
